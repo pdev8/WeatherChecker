8d38a5db97718f911c03cd2322eac96eccc71dc8
Paul Lee <paul.lee@teknowlogi.com>
2018-12-24 20:57:46 -0800

    Brings in functionality for unit testing through MSTest

---
+++ UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Weather
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
